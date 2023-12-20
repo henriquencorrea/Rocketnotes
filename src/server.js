87ff6e3c11b0ce7b52c7fb9e394f05d5ad162cb3
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2023-12-20 10:39:37 -0300

    Added the GET method

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello !")
+})
+
 const PORT = 3333;
 app.listen(PORT, () =>console.log(`Server ir running on Port ${PORT}`));
