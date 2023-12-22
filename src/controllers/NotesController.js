2b2b5ce3cf1d84043ab15da0648059c8491617c1
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2023-12-22 18:24:14 -0300

    Added method to delete created notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const knex = require("../database/knex");
 
 class NotesController{
@@ -46,6 +47,15 @@ class NotesController{
       tags,
       links
     });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("notes").where({ id }).delete();
+
+    return response.json();
+
   }
 }
 
