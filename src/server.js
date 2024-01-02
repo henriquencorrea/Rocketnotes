2704ccb364ca7a192f3dc6d07afb1d793e2b1767
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2024-01-02 16:34:23 -0300

    Configuring server.js to render the image in Insomnia

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 
+const uploadConfig = require("./configs/upload");
+
 const express = require("express");
 const routes = require("./routes");
 
@@ -10,6 +12,8 @@ migrationsRun();
 
 const app = express();
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
