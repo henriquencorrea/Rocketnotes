cf087d26458f2425f586f9dc7aa9ca990d6ac122
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2024-01-02 11:46:39 -0300

    Applying user token to requests

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -2,7 +2,7 @@ const knex = require("../database/knex");
 
 class TagsController {
   async index(request, responde) {
-    const { user_id } = request.params;
+    const user_id = request.user.id;
 
     const tags = await knex("tags")
     .where({ user_id })
